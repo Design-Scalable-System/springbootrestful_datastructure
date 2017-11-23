be8dc427c07040b29212c6a4bd7beb7f22f30b1f
Mustajab Ahmad Akhtar <mmt5080@Mustajab.local>
2017-11-23 12:19:00 +0530

    Changed few configurations

--- src/main/java/com/mustajab/main/Application.java
+++ src/main/java/com/mustajab/main/Application.java
@@ -4,13 +4,17 @@
 package com.mustajab.main;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 
 /**
  * @author Mustajab Akhtar
  *
  */
-@SpringBootApplication
+@Configuration
+@EnableAutoConfiguration
+@ComponentScan
 public class Application {
 
   public static void main(String[] args) {
